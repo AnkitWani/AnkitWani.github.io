0f73584f1d02a20983147008ce3d6349b8bd93f7
AnkitWani <156681882+AnkitWani@users.noreply.github.com>
2024-01-21 19:38:43 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/* script.js */
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
